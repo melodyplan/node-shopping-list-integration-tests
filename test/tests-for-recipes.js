843d085ae5d6f96534677394b0356295e7ae31d2
Melody Plan <melodyplan@gmail.com>
2017-06-28 18:06:10 -0700

    5

--- test/tests-for-recipes.js
+++ test/tests-for-recipes.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const {app, runServer, closeServer} = require('..server')
+const {app, runServer, closeServer} = require('../server');
 
 const should = chai.should();
 
